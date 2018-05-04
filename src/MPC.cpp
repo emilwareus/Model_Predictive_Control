915d2a34f5411b86dc0d85519ccc146b163839ac
Emil Wåreus <emil.wareus47@gmail.com>
2018-05-04 14:25:03 +0800

    Debugging

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -92,10 +92,17 @@ class FG_eval {
       AD<double> delta0 = vars[delta_start + t - 1];
       AD<double> a0 = vars[a_start + t - 1];
 
-      AD<double> f0 = coeffs[0] + coeffs[1] * x0 + 
-              coeffs[2] * CppAD::pow(x0, 2) + coeffs[3] * CppAD::pow(x0, 3);;
-      AD<double> psides0 = CppAD::atan(coeffs[1] + 2 * coeffs[2] *
-                           x0 + 3 * coeffs[3] * CppAD::pow(x0, 2));
+      AD<double> f0 = 0.0; 
+      for (int i = 0; i < coeffs.size(); i++) {
+        f0 += coeffs[i] * CppAD::pow(x0, i);
+      }
+      AD<double> psides0 = 0.0;
+      for (int i = 1; i < coeffs.size(); i++) {
+        psides0 += i*coeffs[i] * CppAD::pow(x0, i-1); 
+      }
+      psides0 = CppAD::atan(psides0);
+
+
 
       fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
       fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
